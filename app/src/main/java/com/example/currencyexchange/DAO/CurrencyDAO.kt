a494d8dee7b1fabf25acccd3f72a12d57df076bc
Mateusz Chyliński <mateusz.chylinski97@gmail.com>
2022-09-02 15:51:24 +0200

    Changed database layout. New database will contains currency name (String), and is base (boolean). Deleted old model class for base currency. Changed DAO, repository, and ViewModel. Changed queries for db.

--- app/src/main/java/com/example/currencyexchange/DAO/CurrencyDAO.kt
+++ app/src/main/java/com/example/currencyexchange/DAO/CurrencyDAO.kt
@@ -1,23 +1,29 @@
 package com.example.currencyexchange.DAO
 
 import androidx.room.*
-import com.example.currencyexchange.Models.BaseCurrencyModel
 import com.example.currencyexchange.Models.CurrencyNamesModel
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CurrencyDAO {
 
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertDefaultCurrency(databaseModel: BaseCurrencyModel)
+    //  Insert default currency, for default, it'll be set as euro. If database will contain an "EUR" value, just replace it with second parameter of the data class object ("isBase") = true
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertDefaultCurrency(databaseModel: CurrencyNamesModel)
+
+    // Insert new currency into database
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertNewCurrency(databaseModel: CurrencyNamesModel)
 
-    @Update
-    fun updateBaseCurrency(baseCurrencyModel: BaseCurrencyModel)
+    //  Get all currency names, and boolean "isBase" to check whenever the value is set as base currency or not (1 = yes / 0 = no)
+    @Query("SELECT currency_name, is_base FROM currency_names")
+    fun getCurrencies(): Flow<List<CurrencyNamesModel>>
 
-    @Query("SELECT base_curr FROM base_currency")
-    fun getBaseCurrency(): Flow<BaseCurrencyModel>
-    @Query("SELECT currency_name FROM currency_names ORDER BY currency_name")
-    fun getAllCurrencies(): Flow<List<CurrencyNamesModel>>
+    //  Get base currency from the database
+    @Query("SELECT currency_name FROM currency_names WHERE is_base = 1")
+    fun getBaseCurrency(): Flow<String>
+
+    //  Update base currency
+    @Update
+    suspend fun updateBaseCurrency(currencyNamesModel: CurrencyNamesModel)
 }
--- app/src/main/java/com/example/currencyexchange/Repository/CurrencyDatabaseRepository.kt
+++ app/src/main/java/com/example/currencyexchange/Repository/CurrencyDatabaseRepository.kt
@@ -2,28 +2,29 @@ package com.example.currencyexchange.Repository
 
 import androidx.annotation.WorkerThread
 import com.example.currencyexchange.DAO.CurrencyDAO
-import com.example.currencyexchange.Models.BaseCurrencyModel
 import com.example.currencyexchange.Models.CurrencyNamesModel
 import kotlinx.coroutines.flow.Flow
 
 class CurrencyDatabaseRepository(private val currencyDAO: CurrencyDAO) {
 
-
-//  Base currency
-    val baseCurrency: Flow<BaseCurrencyModel> = currencyDAO.getBaseCurrency()
-
+    //  Insert new currency into the database
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
-    suspend fun insertNewCurrency(currencyNamesModel: CurrencyNamesModel){
+    suspend fun insertNewCurrency(currencyNamesModel: CurrencyNamesModel) {
         currencyDAO.insertNewCurrency(currencyNamesModel)
     }
 
-//  All currencies
-    val allCurrencyNamesModel: Flow<List<CurrencyNamesModel>> = currencyDAO.getAllCurrencies()
-
+    //  Update base currency
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
-    suspend fun updateBaseCurrency(baseCurrencyModel: BaseCurrencyModel){
-        currencyDAO.updateBaseCurrency(baseCurrencyModel)
+    suspend fun updateBaseCurrency(currencyNamesModel: CurrencyNamesModel) {
+        currencyDAO.updateBaseCurrency(currencyNamesModel)
     }
+
+    //  Get base currency from database
+    val baseCurrency: Flow<String> = currencyDAO.getBaseCurrency()
+
+    //  Get all currencies from the database
+    val allCurrencies: Flow<List<CurrencyNamesModel>> = currencyDAO.getCurrencies()
+
 }
