8cec4c080ad84aa0c6f88c6ce1c01e24673e74cb
Mateusz Chyliński <mateusz.chylinski97@gmail.com>
2023-09-27 00:07:46 +0200

    removed nullable properties/objects

--- app/src/main/java/com/example/currencyexchange/Repository/Interfaces/DatabaseRepository.kt
+++ app/src/main/java/com/example/currencyexchange/Repository/Interfaces/DatabaseRepository.kt
@@ -5,12 +5,12 @@ import com.example.currencyexchange.Models.CurrenciesDatabaseMain
 import kotlinx.coroutines.flow.Flow
 
 interface DatabaseRepository {
-    val baseCurrency: Flow<CurrenciesDatabaseMain>?
-    val currencyData: Flow<CurrenciesDatabaseDetailed>?
-    val currencyListData: Flow<List<CurrenciesDatabaseDetailed>>?
-    val isInit: Flow<Boolean>?
+    val baseCurrency: Flow<CurrenciesDatabaseMain>
+    val currencyData: Flow<CurrenciesDatabaseDetailed>
+    val currencyListData: Flow<List<CurrenciesDatabaseDetailed>>
+    val isInit: Flow<Boolean>
 
-    suspend fun insertCurrencies(currency: CurrenciesDatabaseDetailed?)
-    suspend fun updateBaseCurrency(baseCurrency: CurrenciesDatabaseMain?)
-    suspend fun updateRates(currency: CurrenciesDatabaseDetailed?)
+    suspend fun insertCurrencies(currency: CurrenciesDatabaseDetailed)
+    suspend fun updateBaseCurrency(baseCurrency: CurrenciesDatabaseMain)
+    suspend fun updateRates(currency: CurrenciesDatabaseDetailed)
 }
